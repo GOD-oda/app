0a019c2a2d711b8f74c1d97033ac7038d8cf14d1
GOD-oda <last.pass.oda@gmail.com>
2022-08-29 23:05:14 +0900

    Fix phpdoc

--- src/app/Http/Controllers/Web/CoolWord/Admin/EditController.php
+++ src/app/Http/Controllers/Web/CoolWord/Admin/EditController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Web\CoolWord\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\CoolWord\CoolWord;
-use Illuminate\Http\Request;
 
 class EditController extends Controller
 {
@@ -12,10 +11,8 @@ class EditController extends Controller
      * Handle the incoming request.
      *
      * @param $id
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function __invoke($id, Request $request)
+    public function __invoke($id)
     {
         $model = CoolWord::findOrFail($id);
 
