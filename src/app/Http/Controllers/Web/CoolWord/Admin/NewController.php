7d0e0f9b988b84bf6fd5390d5b407ca51667e883
GOD-oda <last.pass.oda@gmail.com>
2022-08-29 23:03:21 +0900

    Fix phpdoc

--- src/app/Http/Controllers/Web/CoolWord/Admin/NewController.php
+++ src/app/Http/Controllers/Web/CoolWord/Admin/NewController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers\Web\CoolWord\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class NewController extends Controller
 {
     /**
      * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function __invoke()
     {
