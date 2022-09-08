8651060a2a2b35606fa850d87b957b3514e3626c
GOD-oda <last.pass.oda@gmail.com>
2022-09-08 13:29:54 +0900

    Add exception routing for real testing

--- src/routes/api.php
+++ src/routes/api.php
@@ -21,3 +21,7 @@ Route::get('healthcheck', function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('exception', function () {
+    throw new Exception('test', 500);
+});
