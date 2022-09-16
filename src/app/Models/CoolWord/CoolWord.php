abd5475b1559065e2c2dff240832918add753473
GOD-oda <last.pass.oda@gmail.com>
2022-09-16 19:27:15 +0900

    feat: Prepare test db

--- src/app/Models/CoolWord/CoolWord.php
+++ src/app/Models/CoolWord/CoolWord.php
@@ -2,8 +2,12 @@
 
 namespace App\Models\CoolWord;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class CoolWord extends BaseModel
 {
+    use HasFactory;
+
     protected $fillable = [
         'name'
     ];
---
+++ src/database/factories/CoolWord/CoolWordFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\CoolWord;
+
+use App\Models\CoolWord\CoolWord;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CoolWord\CoolWord>
+ */
+class CoolWordFactory extends Factory
+{
+    protected $model = CoolWord::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => 'foo'
+        ];
+    }
+}
