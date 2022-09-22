2b4cc28fb37a4fa099b8c4b8d4e665d52e0db497
GOD-oda <last.pass.oda@gmail.com>
2022-09-22 18:16:15 +0900

    fix: Implement index() instead of forPage()

--- src/app/Models/CoolWord/CoolWord.php
+++ src/app/Models/CoolWord/CoolWord.php
@@ -2,15 +2,16 @@
 
 namespace App\Models\CoolWord;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * App\Models\CoolWord\CoolWord
  *
  * @method static \Database\Factories\CoolWord\CoolWordFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|CoolWord newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|CoolWord newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|CoolWord query()
+ * @method static Builder|CoolWord newModelQuery()
+ * @method static Builder|CoolWord newQuery()
+ * @method static Builder|CoolWord query()
  * @mixin \Eloquent
  */
 class CoolWord extends BaseModel
@@ -20,4 +21,11 @@ class CoolWord extends BaseModel
     protected $fillable = [
         'name'
     ];
+
+    public function scopeName(Builder $query, string $name): void
+    {
+        if (!empty($name)) {
+            $query->where('name', 'like', "%$name%");
+        }
+    }
 }
---
+++ src/tests/Main/CoolWord/Domain/CoolWord/CoolWordCollectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Main\CoolWord\Domain\CoolWord;
+
+use CoolWord\Domain\CoolWord\CoolWord;
+use CoolWord\Domain\CoolWord\CoolWordCollection;
+use CoolWord\Domain\CoolWord\CoolWordId;
+use CoolWord\Domain\CoolWord\Name;
+use Tests\TestCase;
+
+class CoolWordCollectionTest extends TestCase
+{
+    public function testAdd()
+    {
+        $collection = new CoolWordCollection();
+        $coolWord = new CoolWord(
+            id: new CoolWordId(1),
+            name: new Name('foo')
+        );
+        $collection = $collection->add($coolWord);
+
+        $this->assertCount(1, $collection->all());
+    }
+}
