90c2c5a72978b0b74c78c4493bc7e6e8e2ee9d19
GOD-oda <last.pass.oda@gmail.com>
2022-09-25 02:04:43 +0900

    feat: Authentication

---
+++ src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function show()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $guard = Auth::guard();
+
+        $attempt = $guard->attempt(
+            $request->only('email', 'password'),
+            $request->boolean('remember')
+        );
+
+        if ($attempt) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            $request->session()->regenerate();
+
+            return redirect()->route('cool_word.admin.cool_words.index');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans('auth.failed')],
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('cool_word.admin.cool_words.login');
+    }
+}
---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+@extends('base')
+
+@section('main')
+  <div class="container py-3">
+    <form action="{{ route('login.login') }}" method="post">
+      @csrf
+
+      <div class="row mb-3">
+        <label for="email" class="col-md-3 col-form-label text-md-end">Email</label>
+        <div class="col-md-8">
+          <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required>
+          @error('email')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <label for="password" class="col-md-3 col-form-label text-md-end">Password</label>
+        <div class="col-md-8">
+          <input id="password" type="password" class="form-control" name="password" required>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8 offset-md-3">
+          <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </form>
+  </div>
+@endsection
