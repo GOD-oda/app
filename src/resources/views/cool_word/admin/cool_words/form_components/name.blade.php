aa23ac442d06db9b6f3d3e73c63f591bafdbb39c
Workman <GOD-oda@users.noreply.github.com>
2022-10-09 01:20:49 +0900

    fix: Add name property (#36)

--- src/resources/views/cool_word/admin/cool_words/form_components/name.blade.php
+++ src/resources/views/cool_word/admin/cool_words/form_components/name.blade.php
@@ -1,2 +1,2 @@
 <label for="name">Name</label>
-<input type="text" class="form-control" id="name" value="{{ $value }}" @if ($disabled) disabled @endif>
+<input type="text" class="form-control" id="name" name="name" value="{{ $value }}" @if ($disabled) disabled @endif>
