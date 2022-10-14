a707bd8a8e70760fd3361103a7b9f70afe2e94a2
Workman <GOD-oda@users.noreply.github.com>
2022-10-15 02:27:53 +0900

    fix: Sp UI (#51)

--- src/resources/views/cool_word/public/cool_words/index.blade.php
+++ src/resources/views/cool_word/public/cool_words/index.blade.php
@@ -5,15 +5,11 @@
     <div class="container py-3">
       <div class="row">
         @foreach ($paginator->items() as $coolWord)
-          <div class="col-sm-12 col-md-4 my-3 ml-5">
-            <div class="card">
-              <div class="card-body">
-                <h4><a href="{{ route('cool_word.show', ['id' => $coolWord['id']]) }}">{{ $coolWord['name'] }}</a></h4>
-                <div class="d-flex align-items-center">
-                  <span class="material-symbols-outlined">visibility</span>
-                  <span class="ms-2">{{ $coolWord['views'] }}</span>
-                </div>
-              </div>
+          <div class="col-sm-12 col-md-4 card-box">
+            <h4><a href="{{ route('cool_word.show', ['id' => $coolWord['id']]) }}">{{ $coolWord['name'] }}</a></h4>
+            <div class="d-flex align-items-center">
+              <span class="material-symbols-outlined">visibility</span>
+              <span class="ms-2">{{ $coolWord['views'] }}</span>
             </div>
           </div>
         @endforeach
