045efe6757245b75d0b671017e06b8350a38ba94
GOD-oda <last.pass.oda@gmail.com>
2022-10-15 18:01:17 +0900

    fix: Create 30 cool_words with 3 tags

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\CoolWord\CoolWord;
+use App\Models\CoolWord\Tag;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,6 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          User::factory()->admin()->create();
-         CoolWord::factory()->create();
+         CoolWord::factory(30)
+             ->has(Tag::factory()->count(3))
+             ->create();
     }
 }
