4e957e025bf2eddc4e6c47bb063610ab092fa7b8
Workman <GOD-oda@users.noreply.github.com>
2022-10-16 17:26:49 +0900

    feat: Implement TagRepository (#67)
    * feat: Implement TagRepository
    
    * fix: Update phpdoc in eloquent models

--- src/app/Models/CoolWord/Tag.php
+++ src/app/Models/CoolWord/Tag.php
@@ -5,6 +5,23 @@ namespace App\Models\CoolWord;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\CoolWord\Tag
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\CoolWord\TagFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Tag extends BaseModel
 {
     use HasFactory;
